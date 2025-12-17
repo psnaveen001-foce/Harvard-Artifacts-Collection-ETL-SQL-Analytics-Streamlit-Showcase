0772cfd96b00933c29e401ea4d3cb6e65a8187c1
psnaveen001-foce <psnaveen001@gmail.com>
2025-12-17 16:21:09 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,32 @@
+# ===============================
+# API CONFIGURATION
+# ===============================
+
+API_KEY = "REPLACE_YOUR_API_KEY_HERE"
+BASE_URL = "https://api.harvardartmuseums.org/object"
+
+# ===============================
+# DATABASE CONFIGURATION
+# ===============================
+
+DB_USER = "REPLACE_DB_USERNAME"
+DB_PASSWORD = "REPLACE_DB_PASSWORD"
+DB_HOST = "REPLACE_DB_HOST"
+DB_PORT = 4000
+
+ROOT_DATABASE = "test"
+TARGET_DATABASE = "Naveen_Harvard_Artifacts_1"
+
+# ===============================
+# SSL CONFIGURATION
+# ===============================
+
+SSL_CA_PATH = "/content/ca.pem"
+
+# ===============================
+# NGROK CONFIGURATION
+# ===============================
+
+NGROK_AUTH_TOKEN = "REPLACE_NGROK_TOKEN"
+
+STREAMLIT_PORT = 8501
